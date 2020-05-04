1751e9cc4d05dd1ee0892fe489550439750a01b1
sdaveas <sdaveas@gmail.com>
2020-05-04 21:09:16 +0300

    Use local folder as datadir

--- startnode.sh
+++ startnode.sh
@@ -1,1 +1,3 @@
-geth --networkid 4224 --datadir "~/etc/go-ethereum" --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi debug,eth,web3,personal,net,miner --unlock 0 --password ./passwords/account_0 --ipcpath "~/etc/go-ethereum/geth.ipc" --allow-insecure-unlock console
+geth --networkid 4224 --datadir "~/Projects/set-up-geth-2" --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi debug,eth,web3,personal,net,miner --allow-insecure-unlock --unlock 0 --password passwords/account_0 console
+
+# --unlock 0 --password ~/Projects/set-up-geth-2/passwords/account_0 console

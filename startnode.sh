9c511736008eda24d5df13680579303a7d2d9e8a
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-05-29 17:24:03 +0300

    support high gas ops

--- startnode.sh
+++ startnode.sh
@@ -1,3 +1,3 @@
-geth --networkid 4224 --datadir "~/Projects/set-up-geth-2" --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi debug,eth,web3,personal,net,miner --allow-insecure-unlock --unlock 0 --password passwords/account_0 console
+geth --networkid 4224 --datadir "." --nodiscover --rpc --rpcport "8545" --port "30303" --miner.gaslimit "9000000000000" --rpccorsdomain "*" --nat "any" --rpcapi debug,eth,web3,personal,net,miner --allow-insecure-unlock --unlock 0 --password passwords/account_0 console
 
 # --unlock 0 --password ~/Projects/set-up-geth-2/passwords/account_0 console
